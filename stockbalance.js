bfa75da181204a10ce4b5bc3accdf1373c2d5fed
Your Name <munawar.hamid@hotmail.com>
2021-06-15 14:50:30 +0500

    make it better

---
+++ stockbalance.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
+const bodyparser = require('body-parser')
+const sql = require('mssql');
+const app = express();
+
+// set views file
+app.set('views', path.join(__dirname,'views'));
+
+// set view engine
+app.set('view engine','ejs');
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.get('/', async(req,res)=>{
+
+
+const config = {
+    user : 'admin',
+    password :'A@Zmet@l!!123',
+    server:'72.255.34.95',
+    database : 'tile', 
+    options:  {
+     //   trustedconnection: true ,
+    //    enableArithAbort: true ,
+        encrypt : false
+        }
+    
+}
+
+  try {
+      // make sure that any items are correctly URL encoded in the connection string
+      let pool = await sql.connect(config);
+     // console.log('db connected');
+      const result = await pool.request().query('select * from qstockbalancemobile');
+     // console.log('query executed');
+     // console.log(result);
+      res.render('stockbal',{
+      title: 'Using nodejs/express/mssql,Parts List', itemsData:result.recordset });
+      pool.close();
+     // console.log('db connection closed');
+    } catch (err) {
+       // console.log(err);
+    }
+     
+    
+  }); 
+  
+  app.listen(process.env.PORT||5000);
+  // create web server
+ // const  webserver = app.listen(process.env.PORT||5000, function(){
+    //  console.log('Node web server is running');
+ // });
+  
